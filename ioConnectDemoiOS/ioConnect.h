588c6b6d678b6c1a2043d99cb4d3a134a9534e0a
TimberTang <tangming0915@gmail.com>
2024-06-24 18:42:07 +0800

    complete ioconnect

---
+++ ioConnectDemoiOS/ioConnect.h
@@ -0,0 +1,21 @@
+//
+//  ioConnect.h
+//  ioConnectCore
+//
+//  Created by Tang Timber on 2024/6/24.
+//
+
+#ifndef ioConnect_h
+#define ioConnect_h
+#include "include/psa/crypto.h"
+#include "include/utils/iotex_dev_access.h"
+#include "include/utils/LowerS/LowerS.h"
+
+#include "include/jose/jose.h"
+#include "include/dids/dids.h"
+
+#include <stdio.h>
+
+void iotex_io_connect_create_did(void);
+
+#endif /* ioConnect_h */
